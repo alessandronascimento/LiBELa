c2e7dfa53d067ad0af69581121f993b8e8dbd048
Alessandro Nascimento <al.s.nascimento@gmail.com>
2019-09-20 12:32:45 -0300

    Bug fixing

--- trunk/src/LiBELa/Energy2.cpp
+++ trunk/src/LiBELa/Energy2.cpp
@@ -81,7 +81,7 @@ double Energy2::compute_ene(Grid* Grids, Mol2* Lig, vector<vector<double> > lig_
         e = this->compute_ene_from_grids_hardcore(Grids, Lig, lig_xyz, energy_result);
         break;
     case 5:
-        e = this->compute_ene_from_grids_hardcore_solvation(Grids, Lig, lig_xyz);
+        e = this->compute_ene_from_grids_hardcore_solvation(Grids, Lig, lig_xyz, energy_result);
         break;
     }
     return (e);
