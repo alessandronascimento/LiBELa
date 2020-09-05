d9027f8a384561db178eeba8486d724e61c5ab20
alessandronascimento <al.s.nascimento@gmail.com>
2020-09-05 18:39:15 -0300

    Added a new tool: mol2_to_dummy_pqr

---
+++ trunk/src/tools/mol2_to_dummy_pqr.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+#include<cmath>
+#include<string>
+#include <vector>
+#include "../LiBELa/Mol2.cpp"
+#include "../LiBELa/WRITER.cpp"
+#include "../LiBELa/PARSER.cpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    if (argc < 3){
+        printf("Usage: %s mymol.mol2 mymol.pqr.\n", argv[0]);
+        exit(1);
+    }
+    PARSER* Input = new PARSER;
+    Input->atomic_model_ff = "AMBER";
+    Input->output = "mol2_to_pqr";
+    Mol2* Lig = new Mol2(Input, string(argv[1]));
+
+    for (int i=0; i<Lig->N; i++){
+        Lig->charges[i] = 0.0000;
+    }
+
+    for (int i=0; i<Lig->Nres; i++){
+        Lig->resnames[i] = "DUM";
+    }
+
+    WRITER* Writer = new WRITER(Input);
+    Writer->write_pqr(Lig, string(argv[2]));
+    return 0;
+}
